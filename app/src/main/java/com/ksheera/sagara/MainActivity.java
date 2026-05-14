59e6916ce23be2174c9aca1ba2bf5947fa85e2e5
Vinayak-Rajput <Vk.ra.org@gmail.com>
2026-05-14 20:56:41 +0530

    Files Added

---
+++ app/src/main/java/com/ksheera/sagara/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ksheera.sagara;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ksheera.sagara',
+  appName: 'Ksheera Sagara',
+  webDir: 'www'
+};
+
+export default config;
